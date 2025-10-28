ec093b37559f3af6ea2d26f198ff292434079554
Aerith Netzer <aerithnetzer@outlook.com>
2025-10-28 14:11:53 -0500

    Edit analysis file to get threshold sweeps

--- nihil/modeling/analysis.py
+++ nihil/modeling/analysis.py
@@ -1,7 +1,7 @@
+import json
 from pathlib import Path
 import pickle
 from loguru import logger
-import matplotlib.pyplot as plt
 import networkx as nx
 import numpy as np
 import pandas as pd
@@ -9,12 +9,11 @@ import plotly.graph_objects as go
 from sklearn.metrics.pairwise import cosine_similarity
 from tqdm import tqdm
 import typer
-import json
+
 from nihil.config import FIGURES_DIR, MODELS_DIR, PROCESSED_DATA_DIR
 
+THRESHOLDS = [0.5, 0.6, 0.7, 0.8, 0.9]
 app = typer.Typer()
-
-g = nx.Graph()
 
 
 @app.command()
@@ -22,90 +21,69 @@ def unweighted_analysis(
     input_path: Path = PROCESSED_DATA_DIR / "dataset.jsonl",
     model_path: Path = MODELS_DIR / "model.pkl",
 ):
-    df = pd.read_json(input_path, lines=True, dtype={"id": str})
-    logger.info(f"DF Columns: {df.columns}")
-    t = 0.5
-    embeddings = np.vstack(df["embeddings"].values)
-    for _, row in df.iterrows():
-        g.add_node(row["id"], title=row["title"])
-    similarity_matrix = cosine_similarity(embeddings)
+    for t in THRESHOLDS:
+        g = nx.Graph()
+        df = pd.read_json(input_path, lines=True, dtype={"id": str})
+        embeddings = np.vstack(df["embeddings"].values)
+        for _, row in df.iterrows():
+            g.add_node(row["id"], title=row["title"])
+        similarity_matrix = cosine_similarity(embeddings)
 
-    # Get upper triangle (excluding diagonal)
-    upper_triangle = similarity_matrix[np.triu_indices_from(similarity_matrix, k=1)]
-
-    # Create Plotly histogram
-    fig = go.Figure(
-        data=[
-            go.Histogram(
-                x=upper_triangle,
-                nbinsx=100,
-                marker=dict(color="steelblue"),
-                opacity=0.8,
-            )
-        ]
-    )
-
-    # Customize layout
-    fig.update_layout(
-        title="Distribution of Off-Diagonal Cosine Similarities",
-        xaxis_title="Cosine Similarity",
-        yaxis_title="Frequency",
-        template="plotly_white",
-    )
-
-    fig.write_html(FIGURES_DIR / "histogram_unweighted.html")
-    ids = df["id"].to_numpy()
-
-    for x in tqdm(range(len(similarity_matrix)), total=len(similarity_matrix)):
-        for y in range(x + 1, len(similarity_matrix)):
-            j = similarity_matrix[x][y]
-            if j > t:
-                g.add_edge(ids[x], ids[y])
-    logger.info(f"Similarity matrix:\n{similarity_matrix}")
-    logger.info(f"First value: {len(similarity_matrix[0])}")
-    # Generate positions for the nodes
-    pos = nx.spring_layout(g)
-    logger.success(f"Calculated `pos`")
-    # Create a Plotly figure
-    fig = go.Figure()
-
-    # Add edges to the figure
-    for u, v, data in tqdm(g.edges(data=True), desc="Adding edges"):
-        x0, y0 = pos[u]
-        x1, y1 = pos[v]
-        fig.add_trace(go.Scatter(x=[x0, x1], y=[y0, y1], mode="lines", line=dict(color="gray")))
-
-    # Add nodes to the figure
-
-    for node, data in g.nodes(data=True):
-        x, y = pos[node]
-        fig.add_trace(
-            go.Scatter(
-                x=[x],
-                y=[y],
-                mode="markers",
-                marker=dict(size=10),
-                text=data["title"],  # <-- this sets the hover text
-                hoverinfo="text",  # <-- ensures only text is shown
-            )
+        upper_triangle = similarity_matrix[np.triu_indices_from(similarity_matrix, k=1)]
+        # Create Plotly histogram
+        fig = go.Figure(
+            data=[
+                go.Histogram(
+                    x=upper_triangle,
+                    nbinsx=100,
+                    marker=dict(color="steelblue"),
+                    opacity=0.8,
+                )
+            ]
         )
 
-    fig.write_html(FIGURES_DIR / "unweighted_analysis.html")
-    fig.write_image(FIGURES_DIR / "unweighted_analysis.png")
-    rows = []
-    for node, data in g.nodes(data=True):
-        row = df[df["id"] == node].to_dict(orient="records")[0]
-        row["edges"] = list(g.neighbors(node))
-        row["degree"] = g.degree(node)
-        rows.append(row)
+        ids = df["id"].to_numpy()
 
-    # Write to JSON Lines
-    with open("graph_nodes.jsonl", "w") as f:
-        for row in rows:
-            f.write(json.dumps(row) + "\n")
-    with open(model_path, "wb") as f:
-        pickle.dump(g, f)
-    logger.success("Process finished.")
+        for x in tqdm(range(len(similarity_matrix)), total=len(similarity_matrix)):
+            for y in range(x + 1, len(similarity_matrix)):
+                j = similarity_matrix[x][y]
+                if j > t:
+                    g.add_edge(ids[x], ids[y])
+
+        logger.success(f"Density of network with threshold {t}: {nx.density(g)}")
+        logger.success(f"Average path length with threshold {t}: {nx.average_clustering(g)}")
+        # Generate positions for the nodes
+        pos = nx.spring_layout(g)
+        # Create a Plotly figure
+        fig = go.Figure()
+
+        # Add edges to the figure
+        for u, v, data in tqdm(g.edges(data=True), desc="Adding edges"):
+            x0, y0 = pos[u]
+            x1, y1 = pos[v]
+            fig.add_trace(
+                go.Scatter(x=[x0, x1], y=[y0, y1], mode="lines", line=dict(color="gray"))
+            )
+
+        # Add nodes to the figure
+
+        for node, data in g.nodes(data=True):
+            x, y = pos[node]
+            fig.add_trace(
+                go.Scatter(
+                    x=[x],
+                    y=[y],
+                    mode="markers",
+                    marker=dict(size=10),
+                    text=data["title"],  # <-- this sets the hover text
+                    hoverinfo="text",  # <-- ensures only text is shown
+                )
+            )
+
+        fig.write_html(FIGURES_DIR / f"unweighted_analysis_threshold_{t}.html")
+        fig.write_image(FIGURES_DIR / f"unweighted_analysis_threshold_{t}.png")
+
+        logger.success("Process finished.")
 
 
 @app.command()
@@ -113,6 +91,7 @@ def weighted_analysis(
     input_path: Path = PROCESSED_DATA_DIR / "dataset.jsonl",
     model_path: Path = MODELS_DIR / "model.pkl",
 ):
+    g = nx.Graph()
     df = pd.read_json(input_path, lines=True, dtype={"id": str})
     t = 0.999
 
@@ -132,8 +111,6 @@ def weighted_analysis(
                 else:
                     continue
     pos = nx.spring_layout(g, seed=42)
-    logger.info(f"Similarity matrix:\n{similarity_matrix}")
-    logger.info(f"First value: {len(similarity_matrix[0])}")
     logger.success("Process finished.")
     edge_x = []
     edge_y = []
