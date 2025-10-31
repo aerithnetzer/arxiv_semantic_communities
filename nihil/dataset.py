63f8e69b79c98974d56c91d2c4bba59f0ec3c074
Aerith Netzer <aerithnetzer@outlook.com>
2025-10-31 13:43:05 -0500

    Write shards to save memory

--- nihil/dataset.py
+++ nihil/dataset.py
@@ -1,12 +1,12 @@
 from pathlib import Path
+import re
 
 from loguru import logger
 import numpy as np
 import pandas as pd
 from sentence_transformers import SentenceTransformer
 from tqdm import tqdm
-import typer
-import re
+
 from nihil.config import PROCESSED_DATA_DIR, RAW_DATA_DIR
 
 tqdm.pandas()
@@ -33,7 +33,7 @@ def main(
     output_path: Path = PROCESSED_DATA_DIR / "dataset.jsonl",
 ):
     logger.info("Processing dataset...")
-    df_in = pd.read_json(input_path, lines=True, dtype={"id": str}, nrows=1000)
+    df_in = pd.read_json(input_path, lines=True, dtype={"id": str})
     logger.info(f"Loaded dataframe of length {len(df_in)}")
     logger.info(f"Columns of dataframe: {df_in.columns}")
     print(f"Length of df in: {len(df_in)}")
@@ -49,17 +49,21 @@ def main(
     logger.info("Now extracting sentences")
     df["sentences"] = df_in["abstract"].apply(extract_sentences)
     df_exploded = df.explode("sentences", ignore_index=True)
-    df_exploded["sentences"] = df_exploded[
+    df_exploded = df_exploded[
         df_exploded["sentences"].apply(lambda x: isinstance(x, str) and len(x.strip()) > 0)
     ]
     logger.info("Now embedding")
+    df_list = np.array_split(df_exploded, 20)
+    for i, df_split in tqdm(enumerate(df_list), desc="Now creating dataframes", total=20):
+        # Encode each sentence
+        df_split["embeddings"] = list(
+            SENTENCE_TRANSFORMER.encode(df_split["sentences"].tolist(), show_progress_bar=False)
+        )
+        df_split = pd.DataFrame(df_split)
 
-    # Encode each sentence
-    df_exploded["embeddings"] = list(
-        SENTENCE_TRANSFORMER.encode(df_exploded["sentences"].tolist(), show_progress_bar=True)
-    )
-
-    df_exploded.to_json(output_path, orient="records", lines=True)
+        df_split.to_json(
+            f"{PROCESSED_DATA_DIR}/shard_{i:010d}.jsonl", orient="records", lines=True
+        )
 
 
 if __name__ == "__main__":
